de8d864179ec02a30f37111de58ad4d134ef9bd4
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-05-30 08:49:47 +0200

    build react bundle

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
         path: path.join(__dirname, "/view/base/web/js"),
         filename: "vendor.bundle.js"
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /nodeModules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                }
+            },
+        ]
+    }
 }
